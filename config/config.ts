332ca32174ca7fbbabe3e50d3425632c3c80eff1
hitesh.chavda <hitesh.chavada@iflair.com>
2021-03-01 14:31:04 +0530

    did changes in config file

--- config/config.ts
+++ config/config.ts
@@ -5,9 +5,9 @@ export class MongoDB {
   connectMongoDB() {
     try {
       if (ENV.MONGODB_USER && ENV.MONGODB_PASSWORD) {
-        connect(`mongodb+srv://iflairemp245:iflair123@cluster0-sodc3.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`, { useUnifiedTopology: true, useNewUrlParser: true });
+        connect(`your mongo cluster connection link is here`, { useUnifiedTopology: true, useNewUrlParser: true });
       } else {
-        connect(`mongodb+srv://iflairemp245:iflair123@cluster0-sodc3.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`, { useUnifiedTopology: true, useNewUrlParser: true });
+        connect(`your mongo cluster connection link is here`, { useUnifiedTopology: true, useNewUrlParser: true });
       }
     } catch (err) {
       throw err;
